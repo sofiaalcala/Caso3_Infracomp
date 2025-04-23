cc45d655a5986ca2adcb7d35ee61b421fd89d7f5
Juan Felipe Ochoa Bejarano <jf.ochoa@uniandes.edu.co>
2025-04-23 16:42:53 -0500

    Cliente Thread

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,3 +1,17 @@
 public class Cliente {
+
+    public Cliente(String host, int puerto) {
+        //TODO Auto-generated constructor stub
+    }
+
+    public void cargarClavePublica(String archivoClavePublica) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'cargarClavePublica'");
+    }
+
+    public void conectar() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'conectar'");
+    }
     
 }
--- src/ClienteThread.java
+++ src/ClienteThread.java
@@ -1,3 +1,47 @@
-public class ClienteThread {
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
+public class ClienteThread extends Thread {
+    private Cliente cliente;
+    private int id;
+    private String host;
+    private int puerto;
+    private String archivoClavePublica;
     
+    public ClienteThread(int id, String host, int puerto, String archivoClavePublica) {
+        this.id = id;
+        this.host = host;
+        this.puerto = puerto;
+        this.archivoClavePublica = archivoClavePublica;
+    }
+
+
+    public void run() {
+        try{
+            
+            System.out.println("Cliente "+id+" iniciado conexión.");
+
+            cliente =  new Cliente(host, puerto);
+
+            cliente.cargarClavePublica(archivoClavePublica);
+            
+            //conecta con el servidor
+            cliente.conectar();
+
+            System.out.println("Cliente "+id+" finalizado correctamente.");
+            
+        } catch (Exception e) {
+            String errorType;
+            if (e instanceof IOException) {
+                errorType = "Problema de conexión o archivo";
+            } else if (e instanceof GeneralSecurityException) {
+                errorType = "Error criptográfico";
+            } else {
+                errorType = "Error inesperado";
+            }
+            System.err.println("Error en Cliente " + id + ": " + errorType + " - " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+        
 }
